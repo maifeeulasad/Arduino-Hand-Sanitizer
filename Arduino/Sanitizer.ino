28505fcae1fd7400206c90cb53d80df8048c7eec
Maifee Ul Asad <29339330+maifeeulasad@users.noreply.github.com>
2021-06-28 21:50:59 +0600

    added initial delay of 3 seconds

--- Arduino/Sanitizer.ino
+++ Arduino/Sanitizer.ino
@@ -5,6 +5,8 @@ int vcc = 7;          // Actually I needed more pin for VCC
 int trigPin = 11;     // Trigger
 int echoPin = 12;     // Echo
 int t_sanit = 3;
+int initial_sanitize_delay = 3*1000;
+int c_sanitize =0;
 
 void do_sanitize();
 long calculate_distance();
@@ -21,28 +23,26 @@ void setup() {
 void loop() {
   if (calculate_distance() <= 3)
   {
+    delay(initial_sanitize_delay);
     for (int i = 0; i < t_sanit; i++)
     {
       do_sanitize();
     }
-    delay(2000);
+    delay(2*1000);
   }
 }
 
 void do_sanitize() {
   Serial.println("+++++++++++++++++++++++++++++sanitizing");
+  Serial.println(++c_sanitize);
   int t_pos = 90;
   int pos = 0;
   for (pos = 0; pos <= t_pos; pos += 1) {
     myservo.write(pos);
-    //Serial.print(pos);
-    //Serial.print("\t");
     delay(5);
   }
   for (pos = t_pos; pos >= 0; pos -= 1) {
     myservo.write(pos);
-    //Serial.print(pos);
-    //Serial.print("\t");
     delay(5);
   }
 }
